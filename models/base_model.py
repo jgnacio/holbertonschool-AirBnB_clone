93806f8848d67f8bfab8a4807e0dcd53fd788a4e
jgnacio <jgnaciogomez406@gmail.com>
2023-02-22 12:22:32 -0800

    Rewrite function and return a new dict, add kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,7 @@ Created on Wed Feb 22 11:55:00 2023.
             instance.
 """
 
+import json
 import uuid
 from datetime import datetime
 
@@ -24,11 +25,19 @@ from datetime import datetime
 class BaseModel:
     """Base class for creating models."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize the model."""
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        self.id = str(uuid.uuid4())
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Return a human readable string for the model."""
@@ -40,7 +49,8 @@ class BaseModel:
 
     def to_dict(self):
         """Return a dictionary representation of the instance."""
-        self.__dict__['__class__'] = self.__class__.__name__
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        return self.__dict__
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
