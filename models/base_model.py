645f07444cb099b13f9e56d3928ad700593e79e1
jgnacio <jgnaciogomez406@gmail.com>
2023-02-23 09:44:11 -0800

    Fix more 80 characters and add more description

--- models/base_model.py
+++ models/base_model.py
@@ -5,21 +5,30 @@ Created on Wed Feb 22 11:55:00 2023.
 
 @authors: jgnacio & Mauro Trenche
 @description:
-    BaseModel - (class):
+    (class) - BaseModel:
         That is the base class for all models
         of the project.
 
-        save - (method):
+        InstaceAttributes:
+            id: The identifier of the model.
+            created_at: Date of the model was created.
+            updated_at: Date of the model was updated.
+        
+        ImportedAttributes:
+            storage: The main storage variable, that is
+            an instance of the class FileStorage.
+
+        (method) - save():
             Update the last modified time.
 
-        to_dict - (method):
+        (method) - to_dict():
             Return a dictionary representation of the
             instance.
 """
 
-import json
 import uuid
 from datetime import datetime
+from . import storage
 
 
 class BaseModel:
@@ -31,13 +40,16 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                        setattr(self, key, datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                        ))
                     else:
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """Return a human readable string for the model."""
@@ -46,6 +58,7 @@ class BaseModel:
     def save(self):
         """Update date of the model."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Return a dictionary representation of the instance."""
