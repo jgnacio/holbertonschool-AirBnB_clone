3aa57f7ceb7f433a8d4fa131bb465003bd61461e
jgnacio <jgnaciogomez@gmail.com>
2023-02-28 15:24:39 -0300

    Rewrite all, new and reload method. Add more tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,13 +3,10 @@
 """File for testing the FileStorage class."""
 
 import os
+import json
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-from pathlib import Path
-
-# Global variables
-file_storage = FileStorage()
 
 
 def del_old_files():
@@ -19,54 +16,55 @@ def del_old_files():
         pass
 
 
-# Setup files and current objects.
+# Delete files and current objects.
 del_old_files()
+
+# Global variables
+file_storage = FileStorage()
+new_bm = BaseModel()
 
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class."""
 
-    def test_file_path(self):
-        """ Test for FileStorage __file_path attribute"""
-        new_bm = BaseModel()
-        file_storage.save()
-        __file_path = Path('recover_objs.json')
-        assert __file_path.is_file()
-        del_old_files()
-
-    def test_objects(self):
-        """ Test for FileStorage __objects attribute"""
-        new_bm = BaseModel()
-        FileStorage.__objects = {
-            f"{new_bm.__class__.__name__}.{new_bm.id}": new_bm
-        }
-        self.assertEqual(file_storage.__objects, {
-            f"{new_bm.__class__.__name__}.{new_bm.id}": new_bm
-        })
-
     def test_all(self):
         """Test all() method."""
-        self.assertEqual(file_storage.all(), {})
-        new_bm = BaseModel()
+        with open("recover_objs.json", "w", encoding="UTF-8 ") as json_file:
+            json.dump({}, json_file)
+        file_storage.reload()
+        self.assertEqual(file_storage.all(),{})
+        file_storage.new(new_bm)
         self.assertEqual(file_storage.all(), {
             f"{new_bm.__class__.__name__}.{new_bm.id}": new_bm
         })
 
+    def test_file_path(self):
+        """Test __file_path attribue."""
+        self.assertTrue(type(file_storage._FileStorage__file_path) is str)
+
+    def test_objects(self):
+        """Test __objects attribute."""
+        self.assertTrue(type(file_storage._FileStorage__objects) is dict)
+
     def test_new(self):
         """Test creating a new model obj with new method."""
-        new_bm = BaseModel()
-        new_bm.save()
         file_storage.new(new_bm)
-        self.assertNotEqual(file_storage.all(), {
+        new_bm.save()
+        self.assertEqual(file_storage.all(), {
             f"{new_bm.__class__.__name__}.{new_bm.id}": new_bm
         })
 
-    def test_reload(self):
-        """Test reload old objects."""
+    def testReload(self):
+        """Test reloading json file."""
+        file_storage.save()
+        with open("recover_objs.json", "w", encoding="UTF-8") as json_file:
+            json.dump({}, json_file)
+        file_storage.reload()
+        self.assertEqual(file_storage.all(), {})
+        file_storage.new(new_bm)
         file_storage.save()
         file_storage.reload()
-        self.assertNotEqual(file_storage.all(), {})
-        del_old_files()
+        self.assertTrue(len(file_storage.all()) == 1)
 
     def test_save(self):
         """Test save method to save the objects on json file."""
