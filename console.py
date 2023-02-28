3f600193cd55e640930bcc5ddfe063d68dce31de
jgnacio <jgnaciogomez@gmail.com>
2023-02-28 00:31:12 -0300

    Make the basic interactive mode.

--- console.py
+++ console.py
@@ -1,12 +1,56 @@
 #!/usr/bin/python3
-"""interpreter"""
+"""
+Created on Mon Feb 27 10:34:00 2023.
 
+@authors: jgnacio & Mauro Trenche
+@description:
+    (class) - HBNBCommand:
+        Comand interpreter class.
 
-import json
+        InstaceAttributes:
+            prompt: The identifier of the model.
 
-def do_create(self, arg):
-    """new instance of base model"""
-    if len(arg) == 0:
-        print("** class name missing **")
-    else:
+        (method) - do_quit():
+            Quit command to exit the program.
         
+        (method) - do_EOF():
+            EOF command to exit the program.
+
+        (method) - do_create():
+            create command to make a new obj.
+
+        (method) - default():
+            if the read line is empty execute
+            this method, that do nothing.
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """Command class for interactive to objs."""
+
+    cmd.Cmd.prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program."""
+        return True
+
+    def default(self, arg):
+        """Command for empty line."""
+        pass
+
+    def do_create(self, arg):
+        """Make instance of base model."""
+        import json
+
+        if len(arg) == 0:
+            print("** class name missing **")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
